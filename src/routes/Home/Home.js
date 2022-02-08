b278cd70bb45440cbfa1e96aa70aed510b702b02
prince-housing <prince.verma@housing.com>
2022-02-08 19:41:24 +0530

    feat(react-native-reanimated): setup react-matiev animated.

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,7 +1,34 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 import styles from './styles';
+
+function Box() {
+  const offset = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    };
+  });
+
+  const handleButtonPress = () => {
+    offset.value = withSpring(Math.random());
+  };
+
+  return (
+    <View style={styles.animatedBox}>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button onPress={handleButtonPress} title="Move" />
+    </View>
+  );
+}
 
 const Home = props => {
   const { handleTextPress } = props;
@@ -9,6 +36,7 @@ const Home = props => {
     <View style={styles.container}>
       <Text>This is home screen</Text>
       <Text onPress={handleTextPress}>Navigate to Device-info</Text>
+      <Box />
     </View>
   );
 };
--- src/routes/Home/styles.js
+++ src/routes/Home/styles.js
@@ -1,10 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-import { Layouts } from '../../styles';
+import { Colors, Layouts, Spacing } from '../../styles';
 
 export default StyleSheet.create({
+  animatedBox: {
+    ...Layouts.container,
+    marginTop: Spacing.size_16,
+  },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: Colors.Generic.accentBlue,
+    margin: Spacing.size_16,
+  },
   container: {
     ...Layouts.container,
-    ...Layouts.centered,
   },
 });
